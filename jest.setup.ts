c104566270d4989c659f862215379323e27d05d0
Technote <technote.space@gmail.com>
2021-03-04 01:12:18 +0900

    feat: add new example method

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,3 +1,3 @@
-import {setupGlobal} from '@technote-space/github-action-test-helper';
-
-setupGlobal();
+jest.mock('./src/constant', () => Object.assign(jest.requireActual('./src/constant'), {
+  INTERVAL_MS: 0,
+}));
