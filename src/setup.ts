0d943f661adf5de538a01f401ce581a6f6ea1fc2
Technote <technote.space@gmail.com>
2022-04-17 01:42:38 +0900

    chore: add test pattern

--- src/setup.ts
+++ src/setup.ts
@@ -1,5 +1,6 @@
 import { vi } from 'vitest';
 
-vi.mock('./src/constant', () => Object.assign(vi.importActual('./src/constant'), {
-  INTERVAL_MS: 0,
+vi.mock('./constant', async() => ({
+  ...await vi.importActual<{ INTERVAL_MS: number }>('./constant'),
+  WAIT_MS: 0,
 }));
