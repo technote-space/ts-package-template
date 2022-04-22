5f5dd5e00270e272f6e9e38bef675ca06caf700a
Technote <technote.space@gmail.com>
2022-04-22 22:44:15 +0900

    feat: esm, cjs (#175)

---
+++ rollup.config.js
@@ -0,0 +1,26 @@
+import pluginTypescript from '@rollup/plugin-typescript';
+
+const common = {
+  input: 'src/index.ts',
+  external: ['vitest'],
+  plugins: [
+    pluginTypescript(),
+  ],
+};
+
+export default [
+  {
+    ...common,
+    output: {
+      file: 'dist/index.cjs',
+      format: 'cjs',
+    },
+  },
+  {
+    ...common,
+    output: {
+      file: 'dist/index.mjs',
+      format: 'es',
+    },
+  },
+];
